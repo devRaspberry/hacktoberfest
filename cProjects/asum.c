2fc6a3c8f478f2006fa161546b7dbf135f6376d7
Ankesh Kumar <60851047+NoteLyfe@users.noreply.github.com>
2020-10-18 12:14:43 +0530

    Add files via upload

---
+++ cProjects/asum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{int a,b,sum=0;
+printf("Enter no : ");
+scanf("%d",&a);
+printf("Enter no : ");
+scanf("%d",&b);
+sum=a+b;
+printf("sum of %d and %d : %d",a,b,sum);
+getch();
+}
